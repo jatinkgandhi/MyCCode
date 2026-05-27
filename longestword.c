f4ebe690b8657c21ae8b145d443b3ffa3acc73c6
Jatin Gandhi <jatinkgandhi@gmail.com>
2026-05-27 12:45:21 +0530

    Improve longest word detection and add getline
    Refactor SP_GetLongestWord to handle word boundaries and add getline function. If last word is longest, it will not detect it.,

--- longestword.c
+++ longestword.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,6 +6,28 @@
 #define LINE_LENGTH		(255)
 
 int SP_GetLongestWord(char *line, int length, char **longestword);
+
+ssize_t getline(char **lineptr, size_t *n, FILE *stream) {
+    int c;
+    size_t i = 0;
+
+    if (*lineptr == NULL) {
+        *n = 128;
+        *lineptr = malloc(*n);
+    }
+
+    while ((c = fgetc(stream)) != EOF) {
+        if (i + 1 >= *n) {
+            *n *= 2;
+            *lineptr = realloc(*lineptr, *n);
+        }
+        (*lineptr)[i++] = c;
+        if (c == '\n') break;
+    }
+
+    (*lineptr)[i] = '\0';
+    return i ? i : -1;
+}
 
 int main(int argc, char **argv)
 {
@@ -37,6 +60,7 @@ int SP_GetLongestWord(char *line, int length, char **longestword)
 {
 	int maxlength;
 	int current_len;
+    int current_word_len;
 	
 	if(NULL == line || 0 == length || NULL == longestword)
 	{
@@ -44,11 +68,17 @@ int SP_GetLongestWord(char *line, int length, char **longestword)
 	}
 
 	maxlength = current_len = 0;
+    current_word_len = 0;
 	*longestword = line;
 
-	while(NULL != line && '\0' != *line)
+	do
 	{
-		if(' ' == *line || '\t' == *line)
+        #ifdef DEBUG
+        printf("%c", *line);
+        current_word_len++;
+        #endif
+        // Without \n or \0, it will skip the last word. So we need to check for \n and \0 as well.
+		if(' ' == *line || '\t' == *line || '\n' == *line || '\0' == *line)
 		{
 			if(current_len > maxlength)
 			{
@@ -56,16 +86,20 @@ int SP_GetLongestWord(char *line, int length, char **longestword)
 				maxlength = current_len;
 			}
 			current_len = 0;
+            #ifdef DEBUG
+            printf(" : %d\n", current_word_len - 1);
+            current_word_len = 0;
+            #endif
 		}
 		
 		current_len ++;
 		line ++;
-	}
+	}while(NULL != line && '\0' != *line);
 	/* Boundry case single word is passed. */
 	if(0 == maxlength && current_len > 0)
 	{
 	    *longestword = line - current_len;
 	    maxlength = current_len-1; // we run till string terminator '\0';
 	}
-	return maxlength;
+	return maxlength - 1; // exclude the space/tab/newline character/'\0';
 }
